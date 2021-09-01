2feb6b82b0866114375408502644997724c970c9
Igor Dolzhenkov <igor.techweb@gmail.com>
2021-09-01 15:37:34 +0300

    Save

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,82 @@
 import * as ghCore from "@actions/core";
+import {Octokit} from '@octokit/rest'
 
-const filePath = ghCore.getInput("file-path");
+// const filePath = ghCore.getInput("file-path");
 const format = ghCore.getInput("format");
+const githubToken = ghCore.getInput("github-token");
+const githubOwner = ghCore.getInput("github-owner");
+const githubRepo = ghCore.getInput("github-repo");
+const shaBase = ghCore.getInput("sha-base");
+const shaHead = ghCore.getInput("sha-head");
+const pullNumber = ghCore.getInput("pull-number");
 
+function getPattern(format: string) {
+    if (format === 'po') {
+        return /\+msgid "([\w ]*)".*\n\+msgstr.""/
+    }
+    return null
+}
 async function run() {
-    console.log(filePath,format)
+
+    const gitHub = await new Octokit({
+        auth: githubToken
+    })
+
+    const result = await gitHub.repos.compareCommits({
+        owner: githubOwner,
+        repo: githubRepo,
+        base: shaBase,
+        head: shaHead
+    })
+
+    function getMessages(source: string) {
+        const pattern = getPattern(format)
+        if (pattern) {
+            const rex = pattern
+            const re = new RegExp(rex, 'g')
+            const extract = source.match(re)
+            return extract
+                ?.map((text: string) => {
+                    const matches = text.match(rex)
+                    return matches ? matches[1] : ''
+                })
+                ?.filter((text: string) => Boolean(text))
+        }
+        return []
+
+    }
+    if (result) {
+        console.log('result', {
+            result: result?.data?.files?.map((fileData) => ({
+                fileName: fileData.filename,
+                messages: getMessages(fileData?.patch || '')
+            }))
+        })
+
+        const messages = result?.data?.files?.map((fileData) => ({
+            fileName: fileData.filename,
+            messages: getMessages(fileData?.patch || '')
+        }))
+
+        const messagesToPrint = messages?.map(({ fileName, messages}) => {
+            const title = `| ${fileName} |\n| --- |\n`
+            let content: string[] = []
+            messages?.forEach((message) => {
+                content.push(`| ${message} |`)
+            })
+
+            return title + content.join('\n')
+        })
+
+        const comment = messagesToPrint?.join('\n\n')
+
+        gitHub.pulls.createReviewComment({
+            owner: githubOwner,
+            repo: githubRepo,
+            pull_number: +pullNumber,
+            body: comment || '',
+          });
+    }
 }
 
 run()
